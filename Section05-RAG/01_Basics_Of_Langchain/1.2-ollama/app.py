cfcc01d22deb8ff9141d2a345b28543df5f3f3bd
VaibhaV Arde <vaibhav.arde@gmail.com>
2025-11-26 11:45:49 +0530

    SImple app using ollama

--- Section05-RAG/01_Basics_Of_Langchain/1.2-ollama/app.py
+++ Section05-RAG/01_Basics_Of_Langchain/1.2-ollama/app.py
@@ -1,7 +1,7 @@
 import os
 from dotenv import load_dotenv
 
-from langchain_community.llms import Ollama
+from langchain_ollama.llms import OllamaLLM
 import streamlit as st
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
@@ -9,9 +9,9 @@ from langchain_core.output_parsers import StrOutputParser
 load_dotenv()
 
 ## Langsmith Tracking
-os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACING_V2"]="true"
-os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+# os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
+# os.environ["LANGCHAIN_TRACING_V2"]="true"
+# os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
 
 ## Prompt Template
 prompt=ChatPromptTemplate.from_messages(
@@ -27,7 +27,7 @@ input_text=st.text_input("What question you have in mind?")
 
 
 ## Ollama Llama2 model
-llm=Ollama(model="gemma:2b")
+llm=OllamaLLM(model="llama3.1:8b")
 output_parser=StrOutputParser()
 chain=prompt|llm|output_parser
 
